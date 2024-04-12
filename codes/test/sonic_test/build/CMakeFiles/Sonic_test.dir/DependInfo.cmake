5914a1044e67abc6ba8aedaf6c0da53d6ff3da13
rasethna <zaftsethna@outlook.com>
2024-04-12 20:32:07 +0100

    recode test of sonic and motor part from wiringpi to pigpio

---
+++ codes/test/sonic_test/build/CMakeFiles/Sonic_test.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/pi/Desktop/motorsonicweight11122/final!!!!!!!!/Test/sonic/lib/sonic.cpp" "/home/pi/Desktop/motorsonicweight11122/final!!!!!!!!/Test/sonic/build/CMakeFiles/Sonic_test.dir/lib/sonic.cpp.o"
+  "/home/pi/Desktop/motorsonicweight11122/final!!!!!!!!/Test/sonic/lib/sonicmain.cpp" "/home/pi/Desktop/motorsonicweight11122/final!!!!!!!!/Test/sonic/build/CMakeFiles/Sonic_test.dir/lib/sonicmain.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../lib"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
--- codes/test/water_test/water.cpp
+++ codes/test/water_test/water.cpp
@@ -9,18 +9,17 @@ int main() {
     
     // 设置GPIO引脚为输出模式
     // 注意: 这里使用的是WiringPi的引脚编号
-    int pina = 24;
-    int pinb=25; 
+    int pin = 21;
+ 
     pinMode(pina, OUTPUT);
-    pinMode(pinb, OUTPUT);
+   
     
     
     cout << "enter" << endl;
     cin.ignore(); // 等待用户按下回车键
+   
+    digitalWrite(pin, LOW);//low opearte high stop
     
-  
-    digitalWrite(pina, HIGH);
-    digitalWrite(pinb, LOW);
     
     cout << "GPIO high" << endl;
 
