edea80ee41d19ff0977e7bc65240ef0cfa190c7d
rasethna <zaftsethna@outlook.com>
2024-04-14 13:23:45 +0100

    a iot test by bowen

---
+++ codes/test/iot_test/build/CMakeFiles/SmartPet.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SmartPet.dir/lib/SmartPet.cpp.o"
+  "CMakeFiles/SmartPet.dir/main.cpp.o"
+  "SmartPet"
+  "SmartPet.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SmartPet.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ codes/test/iot_test/main.cpp
@@ -0,0 +1,12 @@
+#include "SmartPet.h"
+
+
+int main() {
+    
+
+
+    IOTConnect pet;
+    pet.reportDistance(7.7);
+
+    return 0;
+}
